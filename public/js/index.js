f2b93da9730da8db66470acfe08f1ae4ec6f03ce
Dennis Bozzi <dennisbozzii@gmail.com>
2023-11-07 01:38:57 -0300

    CRUD pronto

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 var divSelecionada;
 var idSelecionado;
 
-exibirLista(1);
+paginacao(0);
 
-function exibirLista(val) {
+function paginacao(val) {
 
     divSelecionada = null
     idSelecionado = null
@@ -11,37 +11,50 @@ function exibirLista(val) {
     var limite = 8;
     var comeco = val * limite;
 
-
-    $.ajax({
+    $.ajax({ //Requisição filtrada
         url: 'http://localhost:3000/pessoas?_start=' + comeco + '&_limit=' + limite,
         type: 'GET',
-        success: function (data) {
+        success: function (dataFiltrada) {
 
-            var divPessoas = document.getElementById('pessoas');
-            var paginacao = document.getElementById('paginacao');
+            $.ajax({ //Requisição completa
+                url: 'http://localhost:3000/pessoas',
+                type: 'GET',
+                success: function (data) {
 
-            variaveluniversal = data;
+                    var divPessoas = document.getElementById('pessoas');
+                    var paginacao = document.getElementById('paginacao');
 
-            divPessoas.innerHTML = "";
-            paginacao.innerHTML = "";
+                    variaveluniversal = dataFiltrada;
 
-            for (let i = 0; i < data.length; i++) {
+                    divPessoas.innerHTML = "";
+                    paginacao.innerHTML = "";
 
-                var id = data[i].id != null ? data[i].id : ""
-                var nome = data[i].nome != null ? data[i].nome : ""
-                var paiId = data[i].paiId != null ? data[i].paiId : "";
-                var maeId = data[i].maeId != null ? data[i].maeId : "";
+                    for (let i = 0; i < dataFiltrada.length; i++) {
 
-                divPessoas.innerHTML += '<div class="btn btn-dark novaPessoa" id="pessoa' + id + '" onclick="selecionado(' + id + ')"><div class="id">' + id + '</div><h3 class="nome">Nome: ' + nome + '</h3><h4 class="pai">Pai: ' + paiId + '</h4><h4 class="mae">Mãe: ' + maeId + '</h4></div>';
+                        var id = dataFiltrada[i].id != null ? dataFiltrada[i].id : "";
+                        var nome = dataFiltrada[i].nome != null ? dataFiltrada[i].nome : "";
+                        var paiId = dataFiltrada[i].paiId != null ? dataFiltrada[i].paiId : "";
+                        var maeId = dataFiltrada[i].maeId != null ? dataFiltrada[i].maeId : "";
 
-            }
+                        var pai = paiId != "" ? data.find(element => element.id == paiId) : "";
+                        var mae = maeId != "" ? data.find(element => element.id == maeId) : "";
 
-            for (let i = 0; i < 5; i++) {
+                        var paiNome = pai != "" && pai != null ? pai.nome : "";
+                        var maeNome = mae != "" && mae != null ? mae.nome : "";
 
-                paginacao.innerHTML += '<button class="btn btn-dark" onclick="exibirLista(' + (i) + ')">' + (i + 1) + '</button>'
+                        divPessoas.innerHTML += '<div class="btn btn-dark novaPessoa" id="pessoa' + id + '" onclick="selecionado(' + id + ')"><div class="id">' + id + '</div><h3 class="nome">Nome: ' + nome + '</h3><h4 class="pai">Pai: ' + paiNome + '</h4><h4 class="mae">Mãe: ' + maeNome + '</h4></div>';
 
-            }
+                    }
 
+                    valMax = data.length / 8
+
+                    for (let i = val >= 1 ? val - 1 : val; i < val + 3 && i < valMax; i++) {
+
+                        paginacao.innerHTML += '<button class="btn btn-dark" onclick="paginacao(' + (i) + ')">' + (i + 1) + '</button>'
+
+                    }
+                }
+            })
         }
     });
 }
@@ -61,6 +74,8 @@ function selecionado(id) {
         return
     }
 
+    popularModal(id)
+
     idSelecionado = id;
 
 }
@@ -78,7 +93,17 @@ function removerPessoa() {
     }
 }
 
-function adicionarPessoa(nome, paiId, maeId) {
+function adicionarPessoa() {
+
+    var nome = $("#inputNome").val();
+    var paiId = $("#inputPaiId").val();
+    var maeId = $("#inputMaeId").val();
+
+
+    if (nome == null || nome == "") {
+        alert('O campo nome é obrigatório!')
+        return
+    }
 
     var novaPessoa = {
         nome: nome,
@@ -88,15 +113,56 @@ function adicionarPessoa(nome, paiId, maeId) {
 
     $.ajax({
         url: 'http://localhost:3000/pessoas',
-        type: 'POST', // Usar o método POST para adicionar uma nova pessoa
-        data: JSON.stringify(novaPessoa), // Enviar a nova pessoa como JSON
-        contentType: 'application/json',
-        success: function (data) {
-            console.log('Pessoa adicionada com sucesso:', data);
-        },
-        error: function (xhr, status, error) {
-            console.error('Erro ao adicionar pessoa:', error);
+        type: 'POST',
+        data: novaPessoa,
+        success: function () {
+            $("#modalAdd").modal('hide');
         }
     });
 }
 
+function popularModal(id) {
+
+    $.ajax({
+        url: 'http://localhost:3000/pessoas',
+        type: 'GET',
+        success: function (data) {
+            var pessoa = data.find(element => element.id === id)
+            $("#tituloModalEdit").html(pessoa.nome)
+            $("#editNome").val(pessoa.nome)
+            $("#editPaiId").val(pessoa.paiId)
+            $("#editMaeId").val(pessoa.maeId)
+        }
+    })
+
+    $('#modalEdit').modal('show');
+}
+
+function editarPessoa() {
+
+    var nome = $("#editNome").val();
+    var paiId = $("#editPaiId").val();
+    var maeId = $("#editMaeId").val();
+
+
+    if (nome == null || nome == "") {
+        alert('O campo nome é obrigatório!')
+        return
+    }
+
+    var pessoaEditada = {
+        nome: nome,
+        paiId: paiId,
+        maeId: maeId
+    };
+
+    $.ajax({
+        url: 'http://localhost:3000/pessoas/' + idSelecionado,
+        type: 'PUT',
+        data: pessoaEditada,
+        success: function () {
+            $('#modalEdit').modal('hide')
+        }
+    })
+
+}
